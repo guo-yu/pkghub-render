e56fe8a2e8eb8d4fadee0fbaa226c1b275da4246
Guo Yu <i@guoyu.me>
2014-02-27 12:22:01 +0800

    0.0.4: move static locals in

--- libs/render.js
+++ libs/render.js
@@ -7,6 +7,10 @@ var isEngine = function(engine) {
     return typeof(engine) === 'function';
 }
 
+var isRemote = function(dir) {
+    return dir && (dir.indexOf('http') === 0 || dir.indexOf('https') === 0);
+}
+
 // 根据给定的主题名称或者文件名称渲染邮件
 // 不指定引擎渲染的话会自动寻找支持的模板引擎
 // e.g: exports.render('mails-flat/message', {...}, callback);
@@ -22,7 +26,10 @@ module.exports = function(template, data, callback, e) {
         var engine = {};
         var dest = file.exist ? file.dir : file.availables[0];
         engine.name = theme['view engine'];
+        // 主题相关信息
         data.Theme = theme;
+        // 渲染页面时要进行 #{static} 变量的替换，这里就是替换成相应主题在 public 下的目录
+        data.static = isRemote(theme.static) ? theme.static : '/' + theme.name;
         try {
             engine._engine = isEngine(e) ? e : require(theme['view engine']);
         } catch (err) {
