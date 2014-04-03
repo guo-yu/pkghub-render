245ba5a7e3b5cbf9b66e85b820d1108f0a43a8f4
Guo Yu <i@guoyu.me>
2014-04-03 11:18:05 +0800

    0.0.6: format codes

--- libs/compile.js
+++ libs/compile.js
@@ -1,5 +1,5 @@
-var fs = require('fs'),
-    errors = require('./errors');
+var fs = require('fs');
+var errors = require('./errors');
 
 // 根据指定模板引擎编译 html 
 // 内置的渲染引擎（即使不提供也能找到的）有 swig 和 jade
@@ -9,13 +9,13 @@ var fs = require('fs'),
 // @engine: 引擎对象，包括 engine.name, engine._engine
 // @callback: 回调函数
 module.exports = function(template, data, engine, callback) {
-    var html;
-    var name = engine.name;
-    var _engine = engine._engine;
-    if (name === 'jade') html = _engine.renderFile(template, data);
-    if (name === 'swig') html = _engine.compileFile(template)(data);
-    if (name === 'ejs') html = _engine.render(fs.readFileSync(template), data);
-    if (!html) return callback(new Error(errors['406']));
-    if (typeof(html) === 'object') return callback(html);
-    return callback(null, html, template, data, engine);
+  var html;
+  var name = engine.name;
+  var _engine = engine._engine;
+  if (name === 'jade') html = _engine.renderFile(template, data);
+  if (name === 'swig') html = _engine.compileFile(template)(data);
+  if (name === 'ejs') html = _engine.render(fs.readFileSync(template), data);
+  if (!html) return callback(new Error(errors['406']));
+  if (typeof(html) === 'object') return callback(html);
+  return callback(null, html, template, data, engine);
 };
