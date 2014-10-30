37b42c5dcfbaa9f45bc35079a7fe43649054dbe3
Guo Yu <i@guoyu.me>
2014-10-30 16:44:29 +0800

    0.1: Add more comments

--- libs/compile.js
+++ libs/compile.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var errors = require('./errors');
 
 module.exports = compile;
 
@@ -11,10 +10,9 @@ module.exports = compile;
 * @param {[String(Path)]} [template] [the template abs path]
 * @param {[Object]} [data] [the template locals]
 * @param {[Object]} [engine] [the view engine's object, contains `engine.name`, `engine._engine`]
-* @param {[Function]} [callback] [the callback function]
 *
 **/
-function compile(template, data, engine, callback) {
+function compile(template, data, engine) {
   var html;
   var name = engine.name;
   var _engine = engine._engine;
@@ -29,11 +27,7 @@ function compile(template, data, engine, callback) {
     html = _engine.render(fs.readFileSync(template), data);
 
   if (!html) 
-    return callback(new Error(errors['406']));
+    throw new Error(406);
 
-  // Error handler
-  if (typeof(html) === 'object') 
-    return callback(html);
-
-  return callback(null, html);
+  return html;
 }
--- libs/render.js
+++ libs/render.js
@@ -5,15 +5,23 @@ var hub = new Hub;
 
 module.exports = renderer;
 
-// 根据给定的主题名称或者文件名称渲染邮件
-// 不指定引擎渲染的话会自动寻找支持的模板引擎
-// e.g: exports.render('mails-flat/message', {...}, callback);
-function renderer(template, data, callback, e) {
+/**
+*
+* A wrapper function for rendering html string by given template and data,
+* Support mutilple view engines.
+*
+* @param {[String]} [template] [template's short name]
+* @example
+*   exports.render('mails-flat/message', {...}, callback);
+*
+**/
+function renderer(template, data, callback) {
   if (!template) 
     return callback(new Error(errors['404']));
 
-  // 加载本地的模块主题
-  hub.load(template, function(err, theme, file) {
+  hub.load(template, loadTemplate);
+
+  function loadTemplate(err, theme, file) {
     if (err) 
       return callback(err);
     if (!theme) 
@@ -23,31 +31,38 @@ function renderer(template, data, callback, e) {
     if (!file) 
       return callback(new Error(errors['404']));
 
-    // 如果匹配不到模板文件，取最相似的第一个
     var engine = {};
-    var dest = file.exist ? file.dir : file.availables[0];
     engine.name = theme['view engine'];
-    // 主题相关信息
+
+    // Select the first file when template file does not exist.
+    var dest = file.exist ? file.dir : file.availables[0];
+
+    // Inject `Theme` locals
     data.Theme = theme;
-    // 渲染页面时要进行 #{static} 变量的替换，这里就是替换成相应主题在 public 下的目录
-    data.static = isUri(theme.static) ? theme.static : '/' + theme.name;
+
+    // Replace #{static} in template with real public path.
+    data.static = isUri(theme.static) ? 
+      theme.static : 
+      '/' + theme.name;
 
     try {
-      engine._engine = isFunction(e) ? e : require(theme['view engine']);
+      engine._engine = require(engine.name);
     } catch (err) {
       return callback(new Error(errors['405']));
     }
 
     try {
-      return compile(dest, data, engine, callback);
+      var html = compile(dest, data, engine);
     } catch (err) {
-      return callback(err);
+      return callback(errors[err.message] || err);
     }
-  });
-}
 
-function isFunction(fn) {
-  return typeof(fn) === 'function';
+    // Errors come from view engine
+    if (typeof(html) === 'object')
+      return callback(html);
+
+    return callback(null, html);
+  }
 }
 
 function isUri(dir) {
