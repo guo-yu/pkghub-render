c809f57fb5d283440f25414e45d54c219591d21c
Guo Yu <i@guoyu.me>
2015-07-22 18:04:16 +0800

    Remove uesless error.js

--- libs/errors.js
+++
@@ -1,6 +0,0 @@
-export default {
-  '403': 'Theme module not found',
-  '404': 'Template file not found',
-  '405': 'Template engine is required',
-  '406': 'Template engine is not supported yet'
-}
